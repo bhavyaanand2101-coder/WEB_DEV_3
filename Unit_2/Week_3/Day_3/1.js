0689983312ddb807df3423905b387b4e7941adc3
bhavyaanand2101-coder <bhavya.anand.2101@gmail.com>
2026-08-19 11:55:23 +0530

    Add

--- Unit_2/Week_3/Day_3/1.js
+++ Unit_2/Week_3/Day_3/1.js
@@ -9,6 +9,8 @@ const express = require("express");
 const fs = require("fs");
 // S-2- Create app
 const app = express();
+// middleware- request body parser
+app.use(express.json());
 // S-3- Create api/routes
 app.get("/", (req, res) => {
   res.send({ msg: "Wlcome to my Website " });
@@ -29,6 +31,30 @@ app.get("/services", (req, res) => {
   const jsData = JSON.parse(data);
   console.log(jsData);
   res.send({ msg: "Services Page" });
+});
+// app.get("/stread", (req, res) => {
+//   const data = fs.readFileSync("1.json", "utf-8");
+//   console.log(data);
+//   console.log(data.students);
+
+//   const jsData = JSON.parse(data);
+//   console.log(jsData.students);
+//   res.send({ msg: "Services Page" });
+// });
+
+// Adding new data-
+app.post("/create", (req, res) => {
+  const payload = req.body;
+  console.log(payload);
+  const data = JSON.parse(fs.readFileSync("1.json", "utf-8"));
+  const stdata = data.students;
+  stdata.push(payload);
+  console.log(stdata);
+  data.students = stdata;
+
+  fs.writeFileSync("1.json", JSON.stringify(data));
+
+  res.send({ msg: "New Student created" });
 });
 // S-4- Run app
 app.listen(8080, () => {
