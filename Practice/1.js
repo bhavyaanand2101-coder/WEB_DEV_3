9e3f0b18487ec209bfca370a8ca5cdf8361a4956
bhavyaanand2101-coder <bhavya.anand.2101@gmail.com>
2026-08-30 19:06:14 +0530

    Add

--- Practice/1.js
+++ Practice/1.js
@@ -2,13 +2,11 @@
 // console.log('Node version:', process.version);    // v24.11.0
 // console.log('Platform:    ', process.platform);   // darwin
 // console.log('PID:         ', process.pid);        // 2505
-// console.log('Working dir: ', process.cwd());      // 
-
+// console.log('Working dir: ', process.cwd());      //
 
 // // Environment variables
 // console.log('PATH:', process.env.PATH);
 // console.log('USER:', process.env.USER);
-
 
 // // Listening to process events
 // process.on('exit', (code) => {
@@ -16,7 +14,6 @@
 // });
 
 // // console.log('Node version:', process.version);
-
 
 // Using .env file with dotenv package:
 
@@ -27,8 +24,18 @@
 // DB_URL=mongodb://...
 
 // In your code:
-require('dotenv').config();
+// require('dotenv').config();
 
-// Then access:
-console.log('PORT:', process.env.PORT);
-console.log('DB_URL:', process.env.DB_URL);
+// // Then access:
+// console.log('PORT:', process.env.PORT);
+// console.log('DB_URL:', process.env.DB_URL);
+
+const http = require("http");
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write("<h1>Hello, World!</h1>");
+  res.end();
+});
+server.listen(8080, () => {
+  console.log("Server running at http://localhost:8080");
+});
