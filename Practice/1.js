bc1625da043965eb80f8f5cba1ac06a742555fa7
bhavyaanand2101-coder <bhavya.anand.2101@gmail.com>
2026-09-20 15:16:34 +0530

    Add

--- Practice/1.js
+++ Practice/1.js
@@ -1,22 +1,46 @@
-const express = require("express");
-const mongoose = require("mongoose");
-require("dotenv").config();
+// const express = require("express");
+// const mongoose = require("mongoose");
+// require("dotenv").config();
 
-const app = express();
-app.use(express.json());
+// const app = express();
+// app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGO_URI;
+// const PORT = process.env.PORT || 5000;
+// const MONGO_URI = process.env.MONGO_URI;
 
-mongoose
-  .connect(MONGO_URI)
-  .then(() => console.log("MongoDB connected successfully"))
-  .catch((err) => console.error("MongoDB connection error:", err));
+// mongoose
+//   .connect(MONGO_URI)
+//   .then(() => console.log("MongoDB connected successfully"))
+//   .catch((err) => console.error("MongoDB connection error:", err));
 
-app.get("/", (req, res) => {
-  res.send("Server is running and MongoDB is connected!");
-});
+// app.get("/", (req, res) => {
+//   res.send("Server is running and MongoDB is connected!");
+// });
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server listening on port ${PORT}`);
+// });
+
+//
+//
+//
+//
+//
+//
+//
+//
+// /
+// /
+// /
+// /
+// /
+//
+//
+//
+//
+// /
+//
+//
+//
+//
+//
